af4f71570e5e3d1c61890605620ec06bf13317ff
Your GitHub Username <davilama@unlv.nevada.edu>
2024-05-14 18:17:06 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -9,13 +9,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'https://images.pexels.com/photos/12561886/pexels-photo-12561886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        pic: '/images/burger.jpg'
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'https://images.pexels.com/photos/8092221/pexels-photo-8092221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        pic: '/images/tacos.jpg'
     }]      
     res.render('places/index', {places})
 })
