db3019a8073d92473f5c43a0a0a193619e4952ec
Your GitHub Username <davilama@unlv.nevada.edu>
2024-05-18 11:42:06 -0700

    added the edit function code but the delete/edit button not showing

--- controllers/places.js
+++ controllers/places.js
@@ -1,44 +1,79 @@
 // The controller area
 const router = require ("express").Router()
 const places = require('../models/places.js')
-
 // Index Route
 router.get('/', (req, res) => {
   res.render('places/index', {places})
 })
-
 // New Route
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/:id', (req, res) => {
-  res.render('places/show')
-})
+// Show Route 
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) { res.render("error404");
+  } else if (!places[id]) { res.render("error404");
+  } else
+  { res.render("places/show", { place: places[id], id: id }); } });
+
+  // Put Route
+  router.put('/:id', (req, res) => {
+    let i = Number(req.params.id)
+    if (isNaN(i)) {
+        res.render('error404')
+    }
+    else if (!places[i]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        // Save the new data into places[i]
+        places[i] = req.body
+        res.redirect(`/places/${i}`)
+    }
+  })  
 
 // Edit route
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
       res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
       res.render('error404')
   }
   else {
-    res.render('/show', { place: places[id] })
     res.render('places/edit', { place: places[id] })
   }
 })
 
-// New Route
-router.get('/new', (req, res) => {
-  res.render('places/new')
+// Delete Route
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
 
-// POST /places
+// Post New Place Route
 router.post('/', (req, res) => {
   // console.log(req.body);
   if (!req.body.pic) {
